9ef481a5460a4ad8161897c1f6d8e69535262e66
ngfchl <ngfchl@126.com>
2018-11-20 22:52:17 +0800

    20181120-22:52

--- src/clock/Display.java
+++ src/clock/Display.java
@@ -1,5 +1,34 @@
 package clock;
 
 public class Display {
-	
+	private int value;
+	private int limit;
+
+	public Display(int limit) {
+		this.limit = limit;
+	}
+
+	public void increase() {
+		value++;
+		if (value == limit) {
+			value = 0;
+		}
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public static void main(String[] args) {
+		Display d = new Display(24);
+		while (true) {
+			d.increase();
+			System.out.println(d.getValue());
+		}
+	}
+
 }
